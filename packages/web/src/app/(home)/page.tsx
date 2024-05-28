673480300a86cd339bc9d901f255c2e028ec7bda
Luan Faria <luuan.fariaf@gmail.com>
2024-05-28 00:19:06 -0300

    add `sass` and `Navbar` component.

---
+++ packages/web/src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={styles.container}>Content</h1>
+    </main>
+  )
+}
---
+++ packages/web/src/types/scss.d.ts
@@ -0,0 +1,6 @@
+// types/scss.d.ts
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
