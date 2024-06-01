3117e52bab6821dc86d58fb8a20730b5e83c89eb
Luan Faria <luuan.fariaf@gmail.com>
2024-05-31 22:27:03 -0300

    fix cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,23 +5,14 @@ const PORT = 3333
 
 const app = express()
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://sevn-monorepo-web.vercel.app'
-];
-const corsOptions: cors.CorsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://sevn-monorepo-web.vercel.app'
+  ]
+}));
 
-app.use(cors(corsOptions));
 app.use(express.json());
-app.options('*', cors(corsOptions));
 app.use(routes)
 
 app.listen(3333, () => {
