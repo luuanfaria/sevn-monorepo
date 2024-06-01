d0278cec8c3ff993827a36fe4d8bf6746da3d010
Luan Faria <luuan.fariaf@gmail.com>
2024-05-31 22:40:24 -0300

    allow preflight

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,13 +5,8 @@ const PORT = 3333
 
 const app = express()
 
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://sevn-monorepo-web.vercel.app'
-  ]
-}));
 
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
