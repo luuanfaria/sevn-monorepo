d58afffb4e8caa79d5cd5d298ae6b312799d07ab
Luan Faria <luuan.fariaf@gmail.com>
2024-05-31 22:06:02 -0300

    remove console.log from api

--- packages/web/src/data/api.ts
+++ packages/web/src/data/api.ts
@@ -1,8 +1,4 @@
 export const api = async (endpoint: string) => {
-  console.log('endpoint: ', endpoint)
-  console.log('env: ', process.env.NEXT_PUBLIC_API_URL)
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`)
-
-  console.log('res: ', res)
   return res
 }
